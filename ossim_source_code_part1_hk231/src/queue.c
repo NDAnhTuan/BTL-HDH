43904ac9c71e8a4a777671c2546a2a952b7e4a40
NDAnh Tuan <nguyenducanhtuan0602@gmail.com>
2023-05-01 10:48:13 +0700

    add en + de

--- ossim_source_code_part1_hk231/src/queue.c
+++ ossim_source_code_part1_hk231/src/queue.c
@@ -7,13 +7,20 @@ int empty(struct queue_t * q) {
 }
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
-	/* TODO: put a new process to queue [q] */	
+	/* TODO: put a new process to queue [q] */
+	q[q -> size] = 	proc;
+	q -> size++;
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	return NULL;
+	struct pcb_t * temp = q[0];
+	for (int i = 0; i < q -> size - 1; i++) {
+		q[i] = q[i + 1];
+	}
+	q[size - 1] = NULL;
+	return temp;
 }
 
