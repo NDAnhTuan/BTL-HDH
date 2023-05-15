b36a4ca24256cc189b16aa04eb97ed7d3dafcab8
Daniel Roussielle <9989840+droussielle@users.noreply.github.com>
2023-05-15 22:38:56 +0700

    format

--- src/queue.c
+++ src/queue.c
@@ -2,28 +2,37 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t * q) {
-        if (q == NULL) return 1;
+int empty(struct queue_t *q)
+{
+	if (q == NULL)
+	{
+		return 1;
+	}
 	return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
 	/* TODO: put a new process to queue [q] */
-	q -> proc[q -> size] = proc;
-	q -> size++;
+	q->proc[q->size] = proc;
+	q->size++;
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
-	/* TODO: return a pcb whose prioprity is the highest
+struct pcb_t *dequeue(struct queue_t *q)
+{
+	/* TODO: return a pcb whose priority is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	if (empty(q)) return NULL;
-	struct pcb_t * temp = q -> proc[0];
-	for (int i = 0; i < q -> size - 1; i++) {
-		q -> proc[i] = q -> proc[i + 1];
+	if (empty(q))
+	{
+		return NULL;
 	}
-	q -> proc[q -> size - 1] = NULL;
-	q -> size--;
+	struct pcb_t *temp = q->proc[0];
+	for (int i = 0; i < q->size - 1; i++)
+	{
+		q->proc[i] = q->proc[i + 1];
+	}
+	q->proc[q->size - 1] = NULL;
+	q->size--;
 	return temp;
 }
-
